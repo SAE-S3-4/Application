030a7c669f6f25b928be78b34ecd6a44f7bdbd84
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2022-10-19 23:49:27 +0200

    Création de la page game

--- src/main/java/sae_s3/findthebreach/Main.java
+++ src/main/java/sae_s3/findthebreach/Main.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("home.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("game.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
         stage.setTitle("Find the breach");
