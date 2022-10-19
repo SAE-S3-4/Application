3dd374cee7a20f736309e9f5c9ee1188ce3557bf
Ceccarelli Luca <luca.ceccarelli@etu.univ-amu.fr>
2022-10-19 08:45:57 +0200

    Ajout du client et du terminal

--- src/main/java/sae_s3/findthebreach/Connection/ClientTerminal.java
+++ src/main/java/sae_s3/findthebreach/Connection/ClientTerminal.java
@@ -1,2 +1,73 @@
-package sae_s3.findthebreach.TerminalClient;public class ClientTerminal {
+package sae_s3.findthebreach.Connection;
+
+import java.io.*;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientTerminal {
+
+    private String hostname;
+    private int port;
+    private Socket socketClient;
+    private BufferedWriter out;
+    private BufferedReader in;
+    private List<String> txtReceived;
+
+    public ClientTerminal(String hostname, int port) {
+        this.hostname = hostname;
+        this.port = port;
+    }
+
+    public void connect() throws UnknownHostException, IOException {
+        socketClient = new Socket(hostname, port);
+        out = new BufferedWriter(new OutputStreamWriter(socketClient.getOutputStream()));
+        in = new BufferedReader(new InputStreamReader(socketClient.getInputStream()));
+    }
+
+    public void send(String msg){
+        try {
+            out.write(msg);
+            // rajouter un saut de ligne pour pouvoir utiliser readline c�t� serveur
+            out.newLine();
+            // vider le buffer
+            out.flush();
+            Thread.sleep(100);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void listen(){
+        txtReceived = new ArrayList<String>();
+
+        Thread receive = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String bufReceived = "";
+                try {
+                    bufReceived = in.readLine();
+
+                    while(bufReceived!=null){
+                        //System.out.println(bufReceived);
+                        txtReceived.add(bufReceived);
+                        bufReceived = in.readLine();
+                    }
+                    System.out.println("Server out of service");
+                    out.close();
+                    socketClient.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        receive.start();
+    }
+
+    public List<String> getBufferReceived(){
+        List<String> buffReceived = new ArrayList<String>(txtReceived);
+        txtReceived = new ArrayList<String>();
+        return buffReceived;
+    }
 }
--- src/main/java/sae_s3/findthebreach/TerminalPane.java
+++ src/main/java/sae_s3/findthebreach/TerminalPane.java
@@ -1,4 +1,54 @@
 package sae_s3.findthebreach;
 
-public class TerminalPane {
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+import sae_s3.findthebreach.Connection.ClientTerminal;
+
+import java.io.IOException;
+import java.util.List;
+
+public class TerminalPane extends BorderPane {
+    public TerminalPane(){
+        super();
+
+        ClientTerminal client = new ClientTerminal("127.0.0.1", 10007);
+        try {
+            client.connect();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        client.listen();
+
+        TextField inputZone = new TextField("");
+
+        TextArea textField = new TextArea("Type help for more information");
+        textField.setEditable(false);
+
+        inputZone.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode().equals(KeyCode.ENTER)) {
+                    //Envoie demande au serv
+                    client.send(inputZone.getText());
+                    //Ecris dans le champ la commande envoyé
+                    textField.appendText("\nUser : "+inputZone.getText());
+                    //Lis la reponse du serv
+                    List<String> buffReceived = client.getBufferReceived();
+                    //Ecris la rep du serv dans la zone de txt
+                    buffReceived.forEach((line) -> textField.appendText("\n"+line));
+                    //Reset
+                    inputZone.setText("");
+                    textField.setScrollTop(Double.MAX_VALUE);
+                }
+            }
+        });
+
+        super.setCenter(textField);
+        super.setBottom(inputZone);
+    }
 }
