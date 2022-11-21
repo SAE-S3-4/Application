4833072576f0c3eaa58592d56e27724d92d2adbe
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2022-11-21 12:07:20 +0100

    Réduction du code pour obtenir le scoreboard

--- src/main/java/fr/univ_amu/iut/ScoreBoardController.java
+++ src/main/java/fr/univ_amu/iut/ScoreBoardController.java
@@ -6,9 +6,10 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 import java.sql.SQLException;
+
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.util.Arrays;
+import java.util.List;
 
 public class ScoreBoardController {
     @FXML
@@ -26,25 +27,21 @@ public class ScoreBoardController {
     @FXML
     Label fifth;
 
-    private ArrayList<User> listUsers = initializePodium();
+    private List<User> listUsers = initializePodium();
 
     public ScoreBoardController() throws SQLException {
     }
 
-    public ArrayList<User> initializePodium() throws SQLException {
-        DAOUsersJDBC daoJDBC = DAOUsersJDBC.getDAOUsersJDBC();
-        ArrayList<User> listUsers;
-        listUsers = (ArrayList<User>) daoJDBC.findAll();
-        Collections.sort(listUsers, Comparator.comparingInt(User::getScore).reversed());
-        return listUsers;
+    public List<User> initializePodium() throws SQLException {
+        ArrayList<User> usersSorted = DAOUsersJDBC.getDAOUsersJDBC().getLeaderBoard();
+        return usersSorted;
     }
 
     @FXML
     public void initialize() throws SQLException {
-        first.setText("1 - " + listUsers.get(0).getNickname());
-        second.setText("2 - " + listUsers.get(1).getNickname());
-        third.setText("3 - " + listUsers.get(2).getNickname());
-        fourth.setText("4 - " + listUsers.get(3).getNickname());
-        fifth.setText("5 - " + listUsers.get(4).getNickname());
+        ArrayList<Label> podiumLines = new ArrayList<Label>(Arrays.asList(first,second,third,fourth,fifth));
+        for (int numLabel = 0; numLabel < 5; numLabel++) {
+            podiumLines.get(numLabel).setText((numLabel + 1) + " - " + listUsers.get(numLabel).getNickname() + " : " + listUsers.get(numLabel).getScore());
+        }
     }
 }
