1dd66d79e2d78182f62202da2b4bf5bc13a9d0a7
Change Me <change-me@example.com>
2022-11-29 22:25:20 +0100

    Ajout du lien entre la fin de jeu et l'onglet LeaderBoard

--- src/main/java/fr/univ_amu/iut/PlayController.java
+++ src/main/java/fr/univ_amu/iut/PlayController.java
@@ -29,32 +29,27 @@ public class PlayController extends BorderPane {
     Label title;
     @FXML
     Label text;
-
     @FXML
     Button homeBtn;
-
     @FXML
     Button practiceMenuBtn;
-
     @FXML
     Button playBtn;
-
     @FXML
     TerminalPane terminalPane;
-
     @FXML
     Button suggestionBtn;
-
     @FXML
     Button solutionBtn;
     @FXML
     HBox botBtns;
-
     private int level;
     private Question question;
+    private String page;
 
     public PlayController(int level, String page, ActionEvent event)  {
         this.level = level;
+        this.page = page;
 
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("fxml/"+page+".fxml"));
         fxmlLoader.setRoot(this);
@@ -64,6 +59,7 @@ public class PlayController extends BorderPane {
         } catch (IOException exception) {
             throw new RuntimeException(exception);
         }
+        //Set the current window to the new node created
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(this,1280, 720);
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
@@ -71,13 +67,12 @@ public class PlayController extends BorderPane {
         stage.setScene(scene);
         stage.show();
 
-        initActions();
-
         question = DAOQuestionJDBC.getDAOQuestionsJDB().findQuestionById(level);
-        System.out.println(question);
 
+        initActions();
         showInstructions();
 
+        //Text Style
         text.setWrapText(true);
         text.setMaxWidth(360);
         text.setMaxHeight(350);
@@ -85,24 +80,53 @@ public class PlayController extends BorderPane {
         title.setWrapText(true);
         title.setMaxWidth(360);
         title.setMaxHeight(200);
-
-        System.out.println(question.getSolution());
-
-        terminalPane.getTextField().textProperty().addListener((observable, oldValue, newValue) -> {
-            String newChunkedValue = newValue.substring(oldValue.length());
-            if(newChunkedValue.contains(question.getSolution())){
-                Button nextQuestion = new Button("Passer à la prochaine Question");
-                nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
-                    @Override public void handle(ActionEvent e) {
-                            new PlayController(level+1,page,e);
-                    }
-                });
-                botBtns.getChildren().setAll(new Button(),nextQuestion);
-            }
-        });
     }
 
     private void initActions(){
+        terminalPane.getTextField().textProperty().addListener((observable, oldValue, newValue) -> {
+            //Get all the old text - all the new text = what has been appended
+            String newChunkedValue = newValue.substring(oldValue.length());
+
+            if(newChunkedValue.contains(question.getSolution())) {
+                Button nextQuestion = new Button();
+                if (level<9){
+                    nextQuestion.setText("Passer à la prochaine question");
+                    nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent e) {
+                            new PlayController(level + 1, page, e);
+                        }
+                    });
+
+                } else if (page.equals("practice")) {
+                    //TODO : Fix l'acces au menu apres avoir fini les questions d'entrainement
+                    nextQuestion.setText("Revenir au Menu");
+                    nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent e) {
+                            try {
+                                switchTo(e,"practice_menu");
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        }
+                    });
+                }else{
+                    nextQuestion.setText("Voir le LeaderBoard");
+                    nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
+                        @Override
+                        public void handle(ActionEvent e) {
+                            try {
+                                switchTo(e,"score");
+                            } catch (IOException ex) {
+                                throw new RuntimeException(ex);
+                            }
+                        }
+                    });
+                }
+                botBtns.getChildren().setAll(new Button(), nextQuestion);
+            }
+        });
         //TODO Creer une classe qui extend Button et lui implementer le setOnAction --> switchTo pour ne pas utiliser le mm controller de partout
         homeBtn.setOnAction(new EventHandler<ActionEvent>() {
             @Override public void handle(ActionEvent e) {
@@ -150,6 +174,19 @@ public class PlayController extends BorderPane {
         Node node = (Node) event.getSource() ;
         data = (String) node.getUserData();
         root = FXMLLoader.load(getClass().getResource(data));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root,1280, 720);
+        scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
+        stage.setTitle("Find the breach");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    public void switchTo(ActionEvent event,String page) throws IOException {
+        Node node = (Node) event.getSource() ;
+        data = (String) node.getUserData();
+        root = FXMLLoader.load(getClass().getResource("fxml/"+page+".fxml"));
         stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root,1280, 720);
         scene.getStylesheets().addAll(this.getClass().getResource("style.css").toExternalForm());
