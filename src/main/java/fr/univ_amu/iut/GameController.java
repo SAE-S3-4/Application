8c9f808f2299b88bc626244187bd0736c6d4031d
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2022-12-13 00:02:58 +0100

    Régulation des solutions et indices

--- src/main/java/fr/univ_amu/iut/GameController.java
+++ src/main/java/fr/univ_amu/iut/GameController.java
@@ -1,16 +1,25 @@
 package fr.univ_amu.iut;
 
 import fr.univ_amu.iut.tools.PlayController;
+
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Class used for the controller of the "game" page
  */
 public class GameController extends PlayController {
+    @FXML
+    Button suggestionBtn;
+
+    @FXML
+    Button solutionBtn;
     private int level;
     private final int MAX_GAME_QUESTIONS = 20;
 
@@ -23,6 +32,27 @@ public class GameController extends PlayController {
     public GameController(int level, ActionEvent event) {
         super(level, "game", event);
         this.level = level;
+        initTimerButtons();
+    }
+
+    /**
+     * Method which allows the management of the display of the buttons solutions
+     * and suggestions according to time
+     */
+    public void initTimerButtons() {
+        Timer timer = new Timer(true);
+        suggestionBtn.setVisible(false);
+        solutionBtn.setVisible(false);
+        timer.schedule(new TimerTask() {
+            public void run() {
+                suggestionBtn.setVisible(true);
+            }
+        }, 300000);
+        timer.schedule(new TimerTask() {
+            public void run() {
+                solutionBtn.setVisible(true);
+            }
+        }, 600000);
     }
 
     /**
@@ -33,11 +63,9 @@ public class GameController extends PlayController {
         getTerminalPane().getTextField().textProperty().addListener((observable, oldValue, newValue) -> {
             //Get all the old text - all the new text = what has been appended
             String newChunkedValue = newValue.substring(oldValue.length());
-
             if(newChunkedValue.contains(getQuestion().getSolution())) {
                 Button nextQuestion = new Button();
                 nextQuestion.setId("nextQuestionButton");
-
                 if (level<MAX_GAME_QUESTIONS){
                     nextQuestion.setText("Passer à la prochaine question");
                     nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
@@ -46,9 +74,11 @@ public class GameController extends PlayController {
                             level = level+1;
                             GameController.super.initLevel(level);
                             getBotBtns().getChildren().setAll(GameController.super.getSuggestionBtn(),GameController.super.getSolutionBtn());
+                            initTimerButtons();
                         }
                     });
                 }else {
+
                     nextQuestion.setText("Passer au score board");
                     nextQuestion.setOnAction(new EventHandler<ActionEvent>() {
                         @Override
