386917506dd88cb0f36770f7a9abf63b2bccab76
Ceccarelli Luca <luca.ceccarelli@etu.univ-amu.fr>
2023-03-26 18:05:20 +0200

    Ajout des elements javaFx pour le chat globale

---
+++ src/main/java/fr/univ_amu/iut/components/ChatPane.java
@@ -0,0 +1,69 @@
+package fr.univ_amu.iut.components;
+
+import fr.univ_amu.iut.LoginController;
+import fr.univ_amu.iut.tools.ClientTerminal;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.BorderPane;
+
+import java.util.List;
+
+import static fr.univ_amu.iut.LoginController.userLogged;
+
+public class ChatPane extends BorderPane {
+    private String reponse;
+    private TextArea textField;
+    private TextField inputZone;
+
+    /**
+     * Constructor of the widget TerminalPane used to create a linux console
+     */
+    public ChatPane(){
+        super();
+        //super.setStyle("-fx-alignment : RIGHT");
+        super.setMaxWidth(600);
+
+        // Initialize the connection
+        ClientTerminal client = new ClientTerminal("findthebreach.sytes.net", 10013);
+        client.connect();
+        client.listen();
+
+        //Initialize the widgets
+        inputZone = new TextField("");
+        inputZone.setStyle("-fx-background-color: transparent");
+        inputZone.setId("terminalInputZone");
+
+        textField = new TextArea("Ecrire dans le chat");
+        textField.setEditable(false);
+        textField.setStyle("-fx-background-color: transparent");
+        textField.setId("terminalTxtField");
+
+        inputZone.setOnKeyPressed(new EventHandler<KeyEvent>() {
+            @Override
+            public void handle(KeyEvent ke) {
+                if (ke.getCode().equals(KeyCode.ENTER)) {
+                    textField.appendText("\n" + userLogged.getName() + " : " + inputZone.getText());
+                    inputZone.setText("");
+                    //Implement the global chat
+                }
+            }
+        });
+
+        Button changeBtn = new Button("⟶");
+        changeBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override public void handle(ActionEvent e) {
+                ChatPane.super.toBack();
+            }
+        });
+
+        super.setCenter(textField);
+        super.setBottom(inputZone);
+        super.setRight(changeBtn);
+    }
+
+}
